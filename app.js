e15a3235fd8b58cc4465b6ca68e04ea63aa23c31
imacoolgirlyo <tjrud1438@naver.com>
2018-08-22 22:01:39 +0900

    Promise.then 첫 성공

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ const express = require('express'),
     app = express(),
     server = http.createServer(app),
     bodyParser = require('body-parser'),
+    fileUpload = require('express-fileupload'),
     db = require('./db'),
     User = require('./model');
    
-
+app.use(fileUpload());
 
 const PORT = process.env.PORT || 8080;;
-
-
 
 app.use(bodyParser.json() ); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/upload', function(req, res){
-    const name = req.body.user,
-        age = req.body.age;
-
-    User.create({
-        name,
-        age
-    }, function(err, res){
-        if(err) return handleError(err);
-       
-    })
-    res.send(`My name is ${name} and I'm ${age}'s old` );
-    
-})
-
-const handleGetUsers = (req, res) => {
-    console.log('Get USER!');
-    User.find().then(messages => res.json({messages}));
+const moveFile = (file, name) => {
+    return new Promise(function(resolve, reject){
+        if(!file){
+            reject(res.statuse(400).send('No files were uploaded'));
+        }
+        else {
+            file.mv('./src/'+ name);
+            resolve(name);
+    }})
 }
 
+app.post('/upload', function(req, res){
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
 
+    let file = req.files.image;
+    let fileName = file.name;
+   
+    moveFile(file, fileName)
+        .then(filename => console.log(filename));
 
-app.get('/users', handleGetUsers);
+    res.send(req.files.mimetype);
+    
+})
 
 const handleListening = ()=>  {
     console.log(`✅ Server Running on : http://localhost:8080`);
