c60fced6cf934291a97112603815a4eecdd102ac
imacoolgirlyo <tjrud1438@naver.com>
2018-08-22 11:57:05 +0900

    Async Ex #1

--- index.js
+++ index.js
@@ -6,54 +6,69 @@ const express = require('express'),
     server = http.createServer(app),
     fileUpload = require('express-fileupload'),
     bodyParser = require('body-parser'),
-    multer = require('multer');
+    multer = require('multer'),
+    Promise = require('promise'),
+    exif   = require(process.cwd() + '/lib/exiftool.js'),
+    expect = require('chai').expect;
 
 
 const PORT = 4000;
-
 
 
 app.use(bodyParser.json() ); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(fileUpload());
-app.get('/topic/:id', function(req, res){
-    const topics = [
-        'JavaScript !',
-        'Node JS !',
-        'Express!'
-    ]
 
-    const output = `
-    <a href="/topic?id=0"> JavaScript</a> </br>
-    <a href="/topic?id=1"> Nodejs</a> </br>
-    <a href="/topic?id=2"> Express</a> </br>
+const MoveFile = (file, name) => {
+    file.mv('./src/' + name)
+}
 
-    ${topics[req.params.id]}
-
-    `
-    res.send(output);
-})
+const readFile = (filename) => {
+   return new Promise(function(resolve, reject){
+    fs.readFile('./src/'+ filename, (err, res)=> {
+        err ? reject(err) : resolve(filename);
+    })
+   });}
 
 
-// app.get('/upload' ,function(req, res){
-//     res.send(req.query.filename);
-// })
+const useExif = (data) => {
+   return new Promise(function(resolve, reject){
+       exif.metadata(data, (err, res) => {
+           err? reject(err) : resolve(console.log(res));
+       })
+   })
+}
 
-app.post('/upload', function(req, res){
+const insertDB = (metadata) => {
+    console.log(metadata);
+}
+   
+
+
+app.post('/upload', function(req, res, next){
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
     let uploadedFile = req.files.filename,
         fileName = uploadedFile.name;
+    
+//    MoveFile(uploadedFile, fileName);
 
-    uploadedFile.mv('./src/'+ fileName, function(err){
-        if(err)
-            return res.status(500).send(err);
+    res.send("file is uploaded !");
 
-        res.send('File uploaded !!');
-    })
-})
+    setTimeout(function(){
+        console.log("this is Async function.");
+    }, 3000);
+
+    console.log('this is console');
+
+
+});
+
+
+
+// 파일이 업로드 되었으면, src파일로 옮기고, then 해당 파일을 exif.metadata로 읽기
 
 
 const handleListening = ()=>  {
